1504973581e3976ee9178d6843c9cc3c784b5ee3
A12ise <sorrawit456flookza@gmail.com>
2025-07-23 16:21:15 +0700

    สร้าง api employee - close #26

--- Frontend/src/services/https/index.tsx
+++ Frontend/src/services/https/index.tsx
@@ -1,6 +1,6 @@
-// import axios, { AxiosError } from "axios";
+import axios from "axios";
 
-const apiUrl = "https://api.nextsut.site";
+const apiUrl = "http://localhost:8000";
 const Authorization = localStorage.getItem("token");
 const Bearer = localStorage.getItem("token_type");
 
@@ -12,7 +12,29 @@ const requestOptions = {
 
 };
 
+async function CreateEmployee(data: any) {
+  return await axios
+    .post(`${apiUrl}/CreateEmployee`, data, requestOptions)
+    .then((res) => res)
+    .catch((e) => e.response);
+}
+
+async function UpdateEmployee(id: number, data: any) {
+  return await axios
+    .patch(`${apiUrl}/UpdateEmployee/${id}`, data, requestOptions)
+    .then((res) => res)
+    .catch((e) => e.response);
+}
+
+async function DeleteEmployeeByID(id: number) {
+  return await axios
+    .delete(`${apiUrl}/DeleteEmployee/${id}`, requestOptions)
+    .then((res) => res)
+    .catch((e) => e.response);
+}
 
 export {
-    // api
+    CreateEmployee,
+    UpdateEmployee,
+    DeleteEmployeeByID,
 };
