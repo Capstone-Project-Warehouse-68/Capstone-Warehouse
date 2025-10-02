d239cb3ee0797b2022e2090e0f2052811c25f47f
A12ise <sorrawit456flookza@gmail.com>
2025-10-02 09:16:10 +0700

    fix download template file

--- Backend/controller/download_file.go
+++ Backend/controller/download_file.go
@@ -8,7 +8,6 @@ import (
 var templateFile []byte
 
 func DownloadTemplate(c *gin.Context) {
-	fileName := "Format_file_DataImport.xlsx"
-	c.Header("Content-Disposition", "attachment; filename="+fileName)
-	c.Data(200, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", templateFile)
+	filePath := "./Format_file_DataImport.xlsx"
+	c.FileAttachment(filePath, "Format_file_DataImport.xlsx")
 }
